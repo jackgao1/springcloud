b69df43b4fc9f58fed6d2e77954b253985cf6d03
jackgao1 <50727687+jackgao1@users.noreply.github.com>
2020-04-23 15:33:58 +0800

    优化

---
+++ server1/src/main/java/com/baidu/server1/controller/ServerController.java
@@ -0,0 +1,23 @@
+package com.baidu.server1.controller;
+
+import com.baidu.server1.service.Server1Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController {
+    @Autowired
+    private Server1Service server1Service;
+
+    @GetMapping("get")
+    private Object get(String msg) {
+        return "这是1的数据"+msg;
+    }
+
+    //服务之间通讯
+    @GetMapping("getServer2")
+    public Object getServer1(String msg) {
+        return server1Service.getServer2(msg);
+    }
+}
---
+++ server1/src/test/java/com/baidu/server1/Server1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baidu.server1;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
