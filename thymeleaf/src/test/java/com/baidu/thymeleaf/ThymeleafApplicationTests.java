df350103d24cee7487b258849782f8e6f4bd8dfe
jackgao1 <50727687+jackgao1@users.noreply.github.com>
2020-05-09 15:07:06 +0800

    添加oauth2服务

--- thymeleaf/src/test/java/com/baidu/thymeleaf/ThymeleafApplicationTests.java
+++ thymeleaf/src/test/java/com/baidu/thymeleaf/ThymeleafApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.apache.commons.codec.binary.Base64;
 
@@ -13,19 +13,23 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ThymeleafApplicationTests {
-    @Autowired
-    public JdbcTemplate jdbcTemplate;
-
-    @Test
-    public void test1() {
-        jdbcTemplate.queryForList("select * from T_GA_FWJBXX");
-    }
-
-    @Test
-    public void test2() {
-        byte[] bytes = jdbcTemplate.queryForObject("select zp from t_syrk_photo where rid='340123196610252084342032510433437'", byte[].class);
-        String s = Base64.encodeBase64String(bytes);
-        System.out.println(s);
+    //    @Autowired
+//    public JdbcTemplate jdbcTemplate;
+//
+//    @Test
+//    public void test1() {
+//        jdbcTemplate.queryForList("select * from T_GA_FWJBXX");
+//    }
+//
+//    @Test
+//    public void test2() {
+//        byte[] bytes = jdbcTemplate.queryForObject("select zp from t_syrk_photo where rid='340123196610252084342032510433437'", byte[].class);
+//        String s = Base64.encodeBase64String(bytes);
+//        System.out.println(s);
+//    }
+    public static void main(String[] args) {
+        String ss = "山西省山西省白水县";
+        System.out.println(ss);
     }
 }
 
---
+++ thymeleaf/target/classes/static/common/common-js.js
@@ -0,0 +1,5 @@
+// 引入公共的js
+document.write('<script src="../js/vue@2.6.20.min.js" type="text/javascript"></script>');
+document.write('<script src="../js/element-ui@2.12.0.min.js" type="text/javascript"></script>');
+// 引入公共的css
+document.write('<link href="../css/element-ui@2.12.0.min.css" rel="stylesheet" type="text/css">');/theme-chalk/index.css">');
